01c6aaa75b8950f73d6ddef536501b62b96f96b2
roshan <customercare@a360pl.com>
2025-05-31 11:22:59 +0530

    testig

--- hooks/useGsapAnimations.js
+++ hooks/useGsapAnimations.js
@@ -22,7 +22,7 @@ export default function useGsapAnimations() {
           scrollTrigger: {
             trigger: el,
             start: "top 90%",
-            toggleActions: "play none none reverse",
+            toggleActions: "play none none reverse roshan",
           },
         }
       );
